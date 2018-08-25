1d7bbd2df23d9ce4c1d051fb1853db98f0e48cbd
msrdinesh <msd11@iitbbs.ac.in>
2018-08-25 21:07:37 +0530

    added delete option to the list

--- linked_list_2.cpp
+++ linked_list_2.cpp
@@ -9,6 +9,7 @@ struct Node
 struct Node* head=NULL;
 void Insert(int,int);
 void print();
+void Delete(int);
 
 int main()
 {
@@ -19,6 +20,9 @@ int main()
 	Insert(5,5);//List:1 2 3 4 5 
 	Insert(0,3);//List:1 2 0 3 4 5
 	Insert(1,1);
+	print();
+	cout<<endl;
+	Delete(4);
 	print();//printing the list
    
 }
@@ -54,4 +58,23 @@ void print()
 		cout<<temp->data<<" ";
 		temp = temp->next;
 	}
+}
+
+void Delete(int n)
+{
+	Node* temp1 = head;
+	if(n==1)
+	{
+		head = temp1->next;
+		delete temp1;
+
+	}
+	for(int i=0;i<n-2;i++)
+	{
+		temp1 = temp1->next;
+
+	}
+
+	temp1->next = (temp1->next)->next;
+	delete temp1->next;
 }
