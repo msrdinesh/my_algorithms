5c1e64440b03d2966765b4e869ea89a2acfff687
msrdinesh <msd11@iitbbs.ac.in>
2018-08-25 20:38:25 +0530

    this is the code to add a node at any position n of a liked list

---
+++ linked_list_2.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+struct Node
+{
+	int data;
+	Node* next;
+};
+
+struct Node* head=NULL;
+void Insert(int,int);
+void print();
+
+int main()
+{
+	Insert(1,1);//List:1
+	Insert(2,2);//List:1 2
+	Insert(3,3);//List:1 2 3
+	Insert(4,4);//List:1 2 3 4 
+	Insert(5,5);//List:1 2 3 4 5 
+	Insert(0,3);//List:1 2 0 3 4 5
+	Insert(1,1);
+	print();//printing the list
+   
+}
+
+void Insert(int data,int n)
+{
+	Node* temp1 = new Node();
+	temp1->data = data;
+	temp1->next = NULL;
+	if(n==1)
+	{
+		temp1->next = head;
+		head = temp1;
+		return;
+	}
+
+	Node* temp2 = head;
+	for(int i=0;i<n-2;i++)
+	{
+		temp2=temp2->next;
+
+	}
+
+	temp1->next = temp2->next;
+	temp2->next = temp1;
+}
+
+void print()
+{
+	Node* temp = head;
+	while(temp!=NULL)
+	{
+		cout<<temp->data<<" ";
+		temp = temp->next;
+	}
+}
